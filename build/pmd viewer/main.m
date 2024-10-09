6f4533074229254085c8aeec7e015535dc4d22dd
anaselii <aninosspro@gmail.com>
2024-10-09 16:32:43 +0100

    UPdate Cmake To SUpport Multiple OS

--- build/pmd viewer/main.m
+++
@@ -1,8 +0,0 @@
-//nya-engine (C) nyan.developer@gmail.com released under the MIT license (see LICENSE)
-
-#import <Cocoa/Cocoa.h>
-
-int main(int argc, char *argv[])
-{
-    return NSApplicationMain(argc, (const char **)argv);
-}
