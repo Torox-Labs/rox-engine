02dd5ddb2a8aae991f84160fa2ace0cc87b9cff7
AnasElii <aninosspro@gmail.com>
2024-10-12 23:53:54 +0100

    Update cmake to support Objective-C++ from Cmake without the need to manually update it each time Update rox_engine file to support WIndows and Other Platforms without droping an error message

--- Engine/rox_engine.h
+++ Engine/rox_engine.h
@@ -1,10 +1,20 @@
 // rox_engine.h
 #pragma once
 
+#ifdef _WIN32
 #ifdef ROX_ENGINE_EXPORTS
 #define ROX_ENGINE_API __declspec(dllexport)
 #else
 #define ROX_ENGINE_API __declspec(dllimport)
+#endif
+#elif defined(__GNU__)
+#ifdef ROX_ENGINE_EXPORTS
+#define ROX_ENGINE_API __attribute__((visibility("default"))
+#else
+#define ROX_ENGINE_API
+#endif
+#else
+#define ROX_ENGINE_API
 #endif
 
 ROX_ENGINE_API void rox_engine_function();
